6e5a61fbaef76036914846028978dd58de74f07d
Kit <chris.codefox@gmail.com>
2017-04-12 01:05:15 -0500

    Fuse stubs

---
+++ src/Caligo/Integrations/Fuse.hs
@@ -0,0 +1,21 @@
+module Caligo.Integrations.Fuse
+( mountCaligo
+) where
+
+import           Control.Monad         (forM)
+
+import           Data.ByteString       (ByteString)
+import qualified Data.ByteString       as B
+import qualified Data.ByteString.Char8 as BC
+import           Data.Maybe            (fromJust)
+
+import           System.Directory
+import           System.Environment    (withArgs)
+import           System.FilePath
+import           System.IO
+
+import           System.Fuse
+
+
+
+mountCaligo = undefined
