597fed34b24619ccb7c89c469e7749881e0a2195
Kit <chris.codefox@gmail.com>
2017-05-01 02:30:44 -0500

    Slightly better logging functions

--- src/Apotheca/Logs.hs
+++ src/Apotheca/Logs.hs
@@ -1,19 +1,24 @@
 module Apotheca.Logs
 ( msg
 , logv
+, logM
 , debug
 , verbose
+, terse
 , warn
 , fatal
 , Verbosity (..)
 ) where
 
-import           Control.Monad (when)
-import           Debug.Trace   (traceIO)
+import           Control.Monad          (when)
+import           Control.Monad.IO.Class
+
+import           Debug.Trace            (traceIO)
 
 data Verbosity
   = Debug
   | Verbose
+  | Terse
   | Warn
   | Fatal
   | Silent
@@ -24,7 +29,11 @@ msg = traceIO
 logv :: Verbosity -> Verbosity -> String -> IO ()
 logv r v s = when (r >= v) $ msg s
 
+logM :: (MonadIO m) => Verbosity -> Verbosity -> String -> m ()
+logM r v s = liftIO $ logv r v s
+
 debug = logv Debug
 verbose = logv Verbose
+terse = logv Terse
 warn = logv Warn
 fatal = logv Fatal
