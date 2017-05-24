d7c0a039ea5b964857823811eba06eaaaf5dc9ea
Kit <chris.codefox@gmail.com>
2017-05-24 11:04:54 -0500

    Slightly more generalized globbing

--- src/Apotheca/Repo/Glob.hs
+++ src/Apotheca/Repo/Glob.hs
@@ -13,13 +13,22 @@ gmatch :: Pattern -> FilePath -> Bool
 gmatch = G.match
 
 glob :: Pattern -> [FilePath] -> [FilePath]
-glob pat = fst . globPart pat
+glob = globT id
 
 globPart :: Pattern -> [FilePath] -> ([FilePath],[FilePath])
-globPart pat fs = L.partition (gmatch pat) fs
+globPart = globPartT id
 
 globNot :: Pattern -> [FilePath] -> [FilePath]
-globNot pat = snd . globPart pat
+globNot = globNotT id
+
+globT :: (a -> FilePath) -> Pattern ->  [a] -> [a]
+globT t pat = fst . globPartT t pat
+
+globPartT :: (a -> FilePath) -> Pattern -> [a] -> ([a],[a])
+globPartT t pat = L.partition (gmatch pat . t)
+
+globNotT :: (a -> FilePath) -> Pattern -> [a] -> [a]
+globNotT t pat = snd . globPartT t pat
 
 globM :: (Monad m) => (Pattern -> [FilePath] -> a) -> Pattern -> m [FilePath] -> m a
 globM g pat paths  = g pat <$> paths
